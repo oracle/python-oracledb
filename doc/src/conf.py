0515f7babcf8beb852b2e1504c33261daac63d95
Anthony Tuininga <anthony.tuininga@gmail.com>
2023-03-29 08:26:09 -0600

    Added sphinx_rtd_theme to extensions.

--- doc/src/conf.py
+++ doc/src/conf.py
@@ -21,7 +21,7 @@ sys.path.append(os.path.abspath("_ext"))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ["table_with_summary", "oracle_deprecated"]
+extensions = ["table_with_summary", "oracle_deprecated", 'sphinx_rtd_theme']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['.templates']
