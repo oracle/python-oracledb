68887bd366ec4697f31d1ca1fc4d54e3bf2b1e75
Anthony Tuininga <anthony.tuininga@gmail.com>
2024-11-13 15:45:32 -0700

    Bump version in preparation for new changes.

--- src/oracledb/version.py
+++ src/oracledb/version.py
@@ -30,4 +30,4 @@
 # file doc/src/conf.py both reference this file directly.
 # -----------------------------------------------------------------------------
 
-__version__ = "2.5.0"
+__version__ = "2.5.1"
